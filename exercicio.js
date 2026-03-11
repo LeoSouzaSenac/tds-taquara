4942c7ccee350bc9f068fe89c2b6b50b033c660f
LUCAS ANDRIESKI AMARAL <03259300074@senacrs.edu.br>
2026-03-11 16:37:15 -0300

    exercicio do Luskinha

--- exercicio.js
+++ exercicio.js
@@ -1,6 +1,13 @@
-let a = 10
-let b = 5
+let a = 10;
+let b = 5;
 
+console.log("Antes da troca:", a, b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("Depois da troca:", a, b);
 /* 
     Desafio:
     Como podemos trocar os valores para que, no final,
