4a46b38206761f5998d4ed03d02afa4b08b39f06
LUCAS ANDRIESKI AMARAL <03259300074@senacrs.edu.br>
2026-03-11 15:16:05 -0300

    scriptLucas

--- scriptLucas.js
+++ scriptLucas.js
@@ -4,3 +4,8 @@ function somar(num1, num2) {
 }
 
 somar(500, 200);
+
+function diminuir(num1, num2) {
+    let resultado = num2 = num2;
+    console.log("A subtração de " + num1 + " e " + num2 + " é " + resultado);
+}
