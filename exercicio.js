e11a3ea78008f90b3e0204b06da0c8f1d1c2a3a5
LUIS FELIPE DREYER BENDER <06494868037@senacrs.edu.br>
2026-03-11 16:34:25 -0300

    exercios do tema

--- exercicio.js
+++ exercicio.js
@@ -1,6 +1,14 @@
-let a = 10
-let b = 5
 
+let a = 10;
+let b = 5;
+
+console.log(" a =", a, "b =", b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(" a =", a, "b =", b);
 /* 
     Desafio:
     Como podemos trocar os valores para que, no final,
